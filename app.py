2162ac085e9637b14da6f32130dbfa9026638e7a
Andrew Laird <alaird4@gmail.com>
2018-05-29 13:09:52 -0700

    Worked on signup form and preferences capture.

--- app.py
+++ app.py
@@ -1,11 +1,47 @@
 from flask import Flask, render_template, jsonify, g, request, flash, redirect, url_for
+import sqlite3
+import json
+from datetime import datetime
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def landing_page():
     return render_template("landing_page.html")
+
+@app.route("/_submit_signup_form", methods=["POST"])
+def submit_signup_data():
+    signup_data = json.loads(request.data)
+    db_conn = sqlite3.connect("leads.db")
+    cursor = db_conn.cursor()
+    data = (datetime.now().timestamp(), signup_data['first_name'], signup_data['last_name'], signup_data['email'])
+    cursor.execute("insert into signups values (?, ?, ?, ?)", data)
+    return jsonify({"success": True, "message": "Signup form submitted successfully."})
+
+@app.route("/_submit_contact_preferences", methods=["POST"])
+def submit_contact_preferences():
+    preferences = json.loads(request.data)
+    db_conn = sqlite3.connect("leads.db")
+    cursor = db_conn.cursor()
+    data = (preferences['email'], preferences['notify'], preferences['updates'], preferences['beta'])
+    cursor.execute("insert into contact_preferences values (?, ?, ?, ?)", data)
+
+    #Fire confirmation email if user chose a notification option
+    if preferences['notify'] or preferences['updates'] or preferences['beta']:
+        send_confirmation_email(preferences)
+
+    return jsonify({"success": True, "message": "Signup form submitted successfully."})
+
+def send_confirmation_email(preferences):
+
+    if preferences['beta']:
+        #We'll get in touch email
+        pass
+    elif preferences['updates'] or preferences['notify']:
+        #We'll keep you posted / unsubscribe
+        pass
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/landing_page.js
+++ static/landing_page.js
@@ -1,15 +1,62 @@
 console.log("landing_page.js loaded successfully.")
 
-let submit_btn = document.getElementById("signup-submit-btn");
+window.onload = function() {
+    console.log("Window loaded.");
 
-console.log(submit_btn);
+    let submit_btn = document.getElementById("signup-submit-btn")
+    console.log(submit_btn);
+    submit_btn.onclick = function() {
+        console.log("Signup submit clicked.");
+        let data = {
+            "first_name": document.getElementsByClassName("signup-first").item(0).value,
+            "last_name": document.getElementsByClassName("signup-last").item(0).value,
+            "email": document.getElementsByClassName("signup-email").item(0).value
+        }
 
-submit_btn.onclick = function() {
-    console.log("Signup submit clicked.");
-    let fields = document.querySelectorAll(".signup-field");
-    let data = [];
+        fetch("/_submit_signup_form", {
+            method: "POST",
+            body: JSON.stringify(data),
+        }).then(response => response.json())
+        .then(jsonData =>
+            swal({
+              title: `Thanks!`,
+              html: `
+              <hr>
+              <div class="contact-modal-text">
+                Choose from the options below to get updates about Mosaic.
+              </div>
+              <div class="contact-modal-options">
+                <input type="checkbox" id="contact-modal-notify"> Notify me when Mosaic is available to the public!</input><br>
+                <input type="checkbox" id="contact-modal-updates"> Send me regular updates about Mosaic. (1 per month or fewer)</input><br>
+                <input type="checkbox" id="contact-modal-beta"> Please contact me about trying a beta version of Mosaic.<br>(permanent discount applies)</input><br>
+              </div>
+              <div class="contact-modal-unsub-notice">You can unsubscribe from our update emails any time.</div>
+              `,
+              type: 'success',
+              confirmButtonColor: '#3085d6',
+              confirmButtonText: 'Continue'
+            }).then((result) => {
+              if (result.value) {
+                post_contact_preferences()
+              }
+            })
+        ).catch(err => {
+            console.log("Signup form data could not be posted to the server.")
+        });
+    };
+};
 
-    fields.forEach(function (field) {
-        data.push()
-    })
-}
+function post_contact_preferences() {
+    let data = {
+        "email": document.getElementsByClassName("signup-email").item(0).value,
+        "notify": document.getElementById("contact-modal-notify").checked,
+        "updates": document.getElementById("contact-modal-updates").checked,
+        "beta": document.getElementById("contact-modal-beta").checked
+    }
+
+    fetch("/_submit_contact_preferences", {
+            method: "POST",
+            body: JSON.stringify(data),
+    }).then(response => response.json())
+    .then(jsonData => console.log(jsonData));
+};
